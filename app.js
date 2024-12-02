1bc4f1bb7e2a7084f17aa14bb34227c954035598
kpilszak <klaudia.pilszak@gmail.com>
2024-12-02 06:12:51 -0500

    Make render responsive

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 function main() {
     const canvas = document.querySelector('#c')
 
-    const fov = 50
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const aspect = width / height
+    const fov = 75
+    const aspect = canvas.clientWidth / canvas.clientHeight
     const near = 0.1
     const far = 2000
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
+    camera.position.z = 1
 
     const renderer = new THREE.WebGLRenderer({ canvas })
-    renderer.setSize(width, height)
+
+    new THREE.OrbitControls(camera, canvas)
 
     const scene = new THREE.Scene()
     const loader = new THREE.TextureLoader()
@@ -24,6 +24,11 @@ function main() {
     })
 
     function render () {
+        const width = canvas.clientWidth
+        const height = canvas.clientHeight
+        camera.aspect = width / height
+        camera.updateProjectionMatrix()
+        renderer.setSize(width, height, false)
         renderer.render(scene, camera)
         requestAnimationFrame(render)
     }
